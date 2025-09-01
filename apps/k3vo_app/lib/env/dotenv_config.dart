3defb31e7db680ecc883498d9f73371547eb28da
kevinomyonga <komyonga@gmail.com>
2025-09-01 21:51:34 +0300

    Fixed web app env var loading error

--- apps/k3vo_app/lib/env/dotenv_config.dart
+++ apps/k3vo_app/lib/env/dotenv_config.dart
@@ -3,8 +3,12 @@ import 'package:k3vo_foundation/k3vo_foundation.dart';
 
 class DotEnvConfig implements Env {
   @override
-  String get k3voApiBaseUrl => dotenv.env['K3VO_API_BASE_URL'] ?? '';
+  String get k3voApiBaseUrl =>
+      dotenv.env['K3VO_API_BASE_URL'] ??
+      const String.fromEnvironment('K3VO_API_BASE_URL', defaultValue: '');
 
   @override
-  String get geminiApiKey => dotenv.env['GEMINI_API_KEY'] ?? '';
+  String get geminiApiKey =>
+      dotenv.env['GEMINI_API_KEY'] ??
+      const String.fromEnvironment('GEMINI_API_KEY', defaultValue: '');
 }
